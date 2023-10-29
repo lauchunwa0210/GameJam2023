655b7bc2ea2effb32722cd426cd9b00ec792ed29
cyberdreamerr <littlekoalalikescola@gmail.com>
2023-10-29 14:31:28 +0000

    add heart image and class entity

--- Game/src/game/sample/entity/Heart.java
+++ Game/src/game/sample/entity/Heart.java
@@ -1,18 +1,24 @@
 package game.sample.entity;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Heart {
     private final Girl girl;
+    private final Image heart = ImageIO.read(new File("Game/resource/image/pixel-heart.png"));
 
-    public Heart(Girl girl) {
+    public Heart(Girl girl) throws IOException {
         this.girl = girl;
     }
 
     public void addBlood(){
-        if (girl.blood <= 90){
-            girl.blood = girl.blood + 10;
+        if (girl.getHealth() <= 90){
+            girl.setHealth(girl.getHealth()+10);
         }
-        if (girl.blood >90){
-            girl.blood = 100;
+        if (girl.getHealth() >90){
+            girl.setHealth(100);
         }
     }
 }
