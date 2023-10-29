4eff8fd9e7d845f504fe95312236296c34440a01
cyberdreamerr <littlekoalalikescola@gmail.com>
2023-10-29 14:45:17 +0000

    update gun entity

--- Game/src/game/sample/entity/Gun.java
+++ Game/src/game/sample/entity/Gun.java
@@ -1,5 +1,10 @@
 package game.sample.entity;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 enum AttackPattern{
     STANDARD,
     SPREAD,
@@ -7,16 +12,39 @@ enum AttackPattern{
 }
 
 public class Gun {
-    int damage;
-    int speed;
+    private final Image gun;
 
-    private void Gun(){
-        this.damage = 10;
-        this.speed = 5;
+    {
+        try {
+            gun = ImageIO.read(new File("Game/resource/image/gun.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    void fire(){
+    int damage = 10;
+    int speed = 5;
 
+    public Gun() {
     }
 
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void fire(){
+
+    }
 }
